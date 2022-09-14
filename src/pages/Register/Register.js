acc092777972d617d85a96cdc844c49334cd7e57
ashkanbossani <abossani@hotmail.com>
2022-09-14 14:30:50 -0700

    completed styling for registration

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Register.scss";
 import { Link } from "react-router-dom";
+import Logo from "../../assets/logo.svg";
 
 function Register(props) {
   const handleSubmit = (e) => {
@@ -10,41 +11,52 @@ function Register(props) {
 
   const handleChange = (e) => {
     console.log(e.target.value);
-    };
+  };
+
   return (
     <>
-      <formm onSubmit={(e) => handleSubmit(e)}>
-        <div className="brand">
-          <img src="" alt="" />
-          <h1>Chatty</h1>
-        </div>
-        <input
-          type="text"
-          placeholder="Username"
-          name="username"
-          onChange={(e) => handleChange(e)}
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          name="email"
-          onChange={(e) => handleChange(e)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          name="password"
-          onChange={(e) => handleChange(e)}
-        />
-        <input
-          type="password"
-          placeholder="Confirm Password"
-          name="confirmPassword"
-          onChange={(e) => handleChange(e)}
-        />
-        <button type="submit">Create User</button>
-        <span>already have an account ? <Link to="/login">Login</Link></span>
-      </formm>
+      <div className="register">
+        <form className="register-form" onSubmit={(e) => handleSubmit(e)}>
+          <div className="register-form__brand">
+            <img className="register-form__img" src={Logo} alt="logo" />
+            <h1 className="register-form__title">Chatty</h1>
+          </div>
+          <input
+            className="register-form__input"
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            className="register-form__input"
+            type="email"
+            placeholder="Email"
+            name="email"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            className="register-form__input"
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            className="register-form__input"
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            onChange={(e) => handleChange(e)}
+          />
+          <button className="register-form__button" type="submit">
+            Create User
+          </button>
+          <span className="register-form__button--text">
+            Already have an account ? <Link to="/login">Login</Link>
+          </span>
+        </form>
+      </div>
     </>
   );
 }
