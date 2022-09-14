571fd62c9ab56af159409c97329a8a39c1b481cc
ashkanbossani <abossani@hotmail.com>
2022-09-14 13:16:21 -0700

    base set up

---
+++ src/pages/Register/Register.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Register.scss";
+
+function Register(props) {
+    return (
+        <div>
+            <p>Register</p>
+        </div>
+    );
+}
+
+export default Register;
