e2c13ff9ef79404ca8f514688fffb6370e5d1425
ashkanbossani <abossani@hotmail.com>
2022-09-15 12:33:48 -0700

    added login info copied from registration changed validation and handle submit to work for login, added login route to utils

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,12 +1,108 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import "./Login.scss";
+import { Link, useNavigate } from "react-router-dom";
+import Logo from "../../assets/logo.svg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { loginRoute } from "../../utils/APIRoutes";
 
-function Login(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+function Login() {
+  const navigate = useNavigate();
+  const [values, setvalues] = useState({
+    username: "",
+    password: "",
+  });
+
+  const toastOptions = {
+    position: "top-center",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (handleValidation()) {
+      const { username, password } = values;
+      const { data } = await axios.post(loginRoute, {
+        username,
+        password,
+      });
+      if (data.status === false) {
+        toast.error(data.message, toastOptions);
+      }
+      if (data.status === true) {
+        localStorage.setItem("user", JSON.stringify(data.user));
+        navigate("/");
+      }
+    }
+  };
+
+  const handleValidation = () => {
+    const { username, password } = values;
+    if (password === "") {
+      toast.error("Email and Password is required", toastOptions);
+      return false;
+    } else if (username.length === "") {
+      toast.error("Email and Password is required", toastOptions);
+      return false;
+    }
+    return true;
+  };
+
+  const handleChange = (e) => {
+    setvalues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <>
+      <div className="register">
+        <form className="register-form" onSubmit={(e) => handleSubmit(e)}>
+          <div className="register-form__brand">
+            <img className="register-form__img" src={Logo} alt="logo" />
+            <h1 className="register-form__title">Chatty</h1>
+          </div>
+          <input
+            className="register-form__input"
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            className="register-form__input"
+            type="email"
+            placeholder="Email"
+            name="email"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            className="register-form__input"
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            className="register-form__input"
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            onChange={(e) => handleChange(e)}
+          />
+          <button className="register-form__button" type="submit">
+            Create User
+          </button>
+          <span className="register-form__button--text">
+            Already have an account ? <Link to="/login">Login</Link>
+          </span>
+        </form>
+      </div>
+      <ToastContainer />
+    </>
+  );
 }
 
 export default Login;
