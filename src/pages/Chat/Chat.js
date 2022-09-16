b28b0d7e5b486d3750341e3537760bf0d12855b8
ashkanbossani <abossani@hotmail.com>
2022-09-16 15:48:50 -0700

    added use effect to retreice data from api for contacts in chat page

--- src/pages/Chat/Chat.js
+++ src/pages/Chat/Chat.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { allUsersRoute } from '../../utils/APIRoutes';
+import Contacts from '../../components/Contacts/Contacts';
 import "./Chat.scss";
 
 function Chat(props) {
+    const [contacts, setContacts] = useState([]);
+    const[currentUser, setCurrentUser] = useState(undefined);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!localStorage.getItem("user")) {
+            navigate("/login");
+          } else {
+           setCurrentUser(JSON.parse(localStorage.getItem("user")));
+          }
+    }, []);
+
+    //if there is current user wil check if avater is set if not will navigate to set avatar page, then will get all contacts and set them to state
+    useEffect(() => {
+        if (currentUser){
+            if(currentUser.isAvatarImageSet){
+                const data = axios.get(`$(allUsersRoute)/${currentUser._id}`);
+                setContacts(data.data);
+            }else {
+                navigate("/avatar");
+            }
+        }
+    }, [currentUser]);
+
+
     return (
         <>
         <div className="chat-container">
             <div className="chat">
+                <Contacts/>
 
             </div>
             
