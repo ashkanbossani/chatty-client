1be0bafd1e5ea3073b7fe66cb563f394b97ae47c
ashkanbossani <abossani@hotmail.com>
2022-09-16 17:02:27 -0700

    added markup for contacts including map and use effect to set current contact info

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,14 +1,66 @@
-import React, {useState} from 'react';
-import "./Contacts.scss"
+import React, { useState, useEffect } from "react";
+import "./Contacts.scss";
 import Logo from "../../assets/logo.svg";
 
+function Contacts({ contacts, currentUser }) {
+  const [currentUserName, setCurrentUserName] = useState(undefined);
+  const [currentUserAvatar, setCurrentUserAvatar] = useState(undefined);
+  const [currentSelected, setCurrentSelected] = useState(undefined);
 
-function Contacts({contacts, currentUser}) {
-    return (
-        <div>
-            <p>contacts</p>
+  useEffect(() => {
+    if (currentUser) {
+      setCurrentUserName(currentUser.username);
+      setCurrentUserAvatar(currentUser.avatarPicture);
+    }
+  }, [currentUser]);
+
+  const changeCurrentChat = (index, contact) => {};
+
+  return (
+    <>
+      {currentUserAvatar && currentUserName && (
+        <div className="contacts-container">
+          <div className="contacts">
+            <img  className="contacts__img" src={Logo} alt="logo" />
+            <h3 className="contacts__title">Chatty</h3>
+          </div>
+          <div className="contacts__contact">
+            {contacts?.map((contact, index) => {
+              return (
+                <div
+                  className={`contact ${
+                    index === currentSelected ? "selected" : ""
+                  }`}
+                  key={index}
+                >
+                  <div className="contact__avatar">
+                    <img
+                      src={`data:image/svg+xml;base64,${contact.avatarPicture}`}
+                      alt="avatar"
+                    />
+                  </div>
+                  <div className="contact__name">
+                    <h3>{contact.username}</h3>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="contacts__current-user">
+            <div className="contacts__current-user__avatar">
+              <img
+                src={`data:image/svg+xml;base64,${currentUserAvatar}`}
+                alt="avatar"
+              />
+            </div>
+            <div className="contact__name">
+              <h2>{currentUserName}</h2>
+            </div>
+          </div>
         </div>
-    );
+      )}
+    </>
+  );
 }
 
 export default Contacts;
--- src/pages/Chat/Chat.js
+++ src/pages/Chat/Chat.js
@@ -1,47 +1,44 @@
-import React, { useState, useEffect} from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { allUsersRoute } from '../../utils/APIRoutes';
-import Contacts from '../../components/Contacts/Contacts';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { allUsersRoute } from "../../utils/APIRoutes";
+import Contacts from "../../components/Contacts/Contacts";
 import "./Chat.scss";
 
-function Chat(props) {
-    const [contacts, setContacts] = useState([]);
-    const[currentUser, setCurrentUser] = useState(undefined);
-    const navigate = useNavigate();
+function Chat() {
+  const [contacts, setContacts] = useState([]);
+  const [currentUser, setCurrentUser] = useState(undefined);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (!localStorage.getItem("user")) {
-            navigate("/login");
-          } else {
-           setCurrentUser( JSON.parse(localStorage.getItem("user")));
-          }
-    }, [navigate]);
+  useEffect(() => {
+    if (!localStorage.getItem("user")) {
+      navigate("/login");
+    } else {
+      setCurrentUser(JSON.parse(localStorage.getItem("user")));
+    }
+  }, [navigate]);
 
-    //if there is current user wil check if avater is set if not will navigate to set avatar page, then will get all contacts and set them to state
-    useEffect(() => {
-        if (currentUser){
-            if(currentUser.isProfilePictureSet){
-                const data = axios.get(`${allUsersRoute}/${currentUser._id}`);
-                setContacts(data.data);
-            }else {
-                navigate("/avatar");
-            }
-        }
-    }, [currentUser]);
+  //if there is current user wil check if avater is set if not will navigate to set avatar page, then will get all contacts and set them to state
+  useEffect(() => {
+    if (currentUser) {
+      if (currentUser.isProfilePictureSet) {
+        const data = axios.get(`${allUsersRoute}/${currentUser._id}`);
+        setContacts(data.data);
+      } else {
+        navigate("/avatar");
+      }
+    }
+  }, [currentUser]);
 
-
-    return (
-        <>
-        <div className="chat-container">
-            <div className="chat">
-                <Contacts contacts={contacts} currentUser={currentUser}/>
-
-            </div>
-            
+  return (
+    <>
+      <div className="chat-container">
+        <div className="chat">
+          <Contacts contacts={contacts} currentUser={currentUser} />
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 }
 
 export default Chat;
