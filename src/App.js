abec6cfc5898c7bf72a2e4fb61b3ee36c11de872
ashkanbossani <abossani@hotmail.com>
2023-01-09 14:15:51 -0800

    added suspense and lazy to decrease bundle size and incfrease optimmization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
+import React, { Suspense, lazy} from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Register from './pages/Register/Register';
-import Chat from './pages/Chat/Chat';
-import Login from './pages/Login/Login';
-import Avatar from './pages/Avatar/Avatar';
 
 const Chat = lazy(() => import('./pages/Chat/Chat'));
 const Login = lazy(() => import('./pages/Login/Login'));
 const Register = lazy(() => import('./pages/Register/Register'));
+const Avatar = lazy(() => import('./pages/Avatar/Avatar'));
 
 function App(props) {
   return (
     <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/register" element={<Register/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/avatar" element={<Avatar/>} />
         <Route path="/" element={<Chat/>} />
       </Routes>
+      </Suspense>
     </BrowserRouter>
 
   );
